7afdbea856c05203b58a742b97b3701d91bbe16b
Nuzzling <antmations@gmail.com>
2023-05-01 12:47:39 +0200

    lowercase names

--- javascript/backend.js
+++ javascript/backend.js
@@ -18,12 +18,63 @@ $(document).ready(function() {
     }
     function getCellValue(row, index){ return $(row).children('td').eq(index).text() }
 
+    $("body").on("click", ".writetext__insert", function() {
+        insertIntoWritetextField($(this).data("insert"));
+    });
+    $(".writetext").on("input", ".writetext__field", function() {
+        loadTextPreview();
+    });
+    $("body").on("click", ".writetext__copy", function() {
+        navigator.clipboard.writeText($(".writetext__field").val().replace(/(\r\n|\n|\r)/gm, ""));
+    });
+    $("body").on("click", ".js-copymydata", function() {
+        navigator.clipboard.writeText($(this).data('tocopy').replace(/(\r\n|\n|\r)/gm, ""));
+    });
+    $("body").on("click", ".js-markonclick", function() {
+        $(".clicked").removeClass("clicked");
+        $(this).addClass("clicked");
+    });
 
 
-
-
-
-
-
-    
 });
+
+function insertIntoWritetextField(toInsert) {
+    let currentValue = $(".writetext__field").val();
+    $(".writetext__field").val(currentValue + toInsert);
+    loadTextPreview();
+    $(".writetext__field").focus();
+}
+
+function loadTextPreview() {
+    let text = $(".writetext__field").val();
+    text = text.replace(/(\r\n|\n|\r)/gm, "");
+
+    const boxHtml = '<div class="writetext__example">';
+    const lineHtml = '<div class="writetext__exampleline">';
+    const scrollHtml = '<span class="writetext__examplescroll">A</span>';
+
+    let output = boxHtml + lineHtml;
+    for(let i = 0; i < text.length; i++) {
+        if(text[i] == "\\") {
+            let special = text[i];
+            i++;
+            special += text[i];
+            if(special == "\\n") {
+                output += "</div>" + lineHtml;
+                continue;
+            } else if(special == "\\f") {
+                output += scrollHtml + "</div>" + lineHtml;
+                continue;
+            } else if(special == "\\r") {
+                output += '</div></div>' + boxHtml + lineHtml;
+                continue;
+            } else {
+                i--;
+            }
+        }
+        output += '<span class="writetext__examplechar">' + text[i] + '</span>';
+    }
+    output += '</div></div>';
+
+    $(".writetext__exampleholder").html(output);
+}
